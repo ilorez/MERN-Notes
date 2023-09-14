206723c180e4c7e470ddf029dad65acd939b405d
Z-Najdaoui <zobirnajdaoui@gmail.com>
2023-09-14 11:25:34 +0100

    express routing 404 redirect

--- NodeJs/lesson 4 $ express/app.js
+++ NodeJs/lesson 4 $ express/app.js
@@ -1,6 +1,30 @@
 const express = require('express');
 const app = express()
-app.get('/', (req, res) => {
-    res.send("<p>Hello world</p>");
-})
 app.listen(3000)
+app.get('/', (req, res) => {
+    // res.send("<p>Hello world</p>");
+    res.sendFile('./data/page.html', { root: __dirname });
+
+})
+
+app.get('/about', (req, res) => {
+    // res.send("<p>Hello world</p>");
+    res.sendFile('./data/about.html', { root: __dirname });
+
+})
+
+// redirect
+app.get('/about-me', (req, res) => {
+    res.redirect('/about')
+})
+
+// 404 page
+app.use((req, res) => {
+    res.status(404).sendFile('./data/404.html', { root: __dirname })
+})
+
+
+
+
+
+
